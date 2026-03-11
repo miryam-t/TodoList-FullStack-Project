8ae5bbe528d23911b0a72594fa62ba19db4faa7c
Miryam T <miryamt05271@gmail.com>
2026-03-11 14:52:07 +0200

    MYSQL final

--- TodoApi/User.cs
+++ TodoApi/User.cs
@@ -1,5 +1,7 @@
+using System.ComponentModel.DataAnnotations.Schema; // הוסיפי את ה-using הזה
 namespace TodoApi;
 
+[Table("Users")] // זה מבטיח שהקוד יפנה בדיוק לטבלה עם ה-U הגדולה וה-s
     public class User
     {
         public int Id { get; set; }
